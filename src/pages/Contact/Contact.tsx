a5eec88c1c58aa8ceaf21cb8113f3ad2f87199cb
Makcim <adoberoflit@gmail.com>
2024-11-12 18:02:16 +0300

    start contact

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,13 +1,17 @@
 // import React from "react";
 import styles from "./Contact.module.scss";
+import { Header } from "../../components";
 // import bg from "../../assets/Contact/bg_contact.webp";
+import prevImg from "../../assets/Contact/prev.png";
 
 export const Contact = () => {
 	return (
-		<div style={{ color: "#000", height: "100%" }}>
-			<div className={styles.background}>
-				{/* <img src={bg} alt="background" /> */}
+		<div className={styles.contact}>
+			<Header />
+			<div className={styles.prevImg__wrapper}>
+				<img src={prevImg} alt="prevImg" />
 			</div>
+			<div className={styles.background}></div>
 		</div>
 	);
 };
